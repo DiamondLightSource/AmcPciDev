428e586e2e84ecdb1c7bfc8efc8f39e22a978345
Michael Abbott <michael.abbott@diamond.ac.uk>
2017-10-20 12:43:23 +0100

    Add size code to LMBF ioctl definitions to help valgrind

--- driver/amc525_lmbf_device.h
+++ driver/amc525_lmbf_device.h
@@ -1,11 +1,16 @@
 /* Header file for use from userspace. */
 
+/* Although our ioctls don't transfer any data, use the direction hint anyway:
+ * this helps valgrind which otherwise complains about missing size hints, and
+ * it doesn't seem to mind the zero size code. */
+#define LAMC_IOCTL(n)       _IOC(_IOC_WRITE, 'L', n, 0)
+
 /* Returns size of register area as unsigned 32-bit integer. */
-#define LAMC_MAP_SIZE       _IO('L', 0)
+#define LAMC_MAP_SIZE       LAMC_IOCTL(0)
 
 /* Locks and unlocks access to register area for exclusive access by caller. */
-#define LAMC_REG_LOCK       _IO('L', 2)
-#define LAMC_REG_UNLOCK     _IO('L', 3)
+#define LAMC_REG_LOCK       LAMC_IOCTL(2)
+#define LAMC_REG_UNLOCK     LAMC_IOCTL(3)
 
 /* Returns size of DMA buffer. */
-#define LAMC_BUF_SIZE       _IO('L', 1)
+#define LAMC_BUF_SIZE       LAMC_IOCTL(1)
